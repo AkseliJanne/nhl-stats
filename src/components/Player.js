07cb8a289ae684e9346c609107f7d45539d4f203
ReactJS Programmer <reactweatherapp@outlook.com>
2020-11-23 11:45:48 +0200

    message

--- src/components/Player.js
+++ src/components/Player.js
@@ -38,14 +38,14 @@ function Player(props) {
             {person &&
                 <div>
                     <Card className={classes.card}>
-                    <Typography variant="h5" color="primary">Name: {person.fullName}</Typography>
-                    <Typography variant="h5" color="primary">Birth City: {person.birthCity}</Typography>
-                    <Typography variant="h5" color="primary">Birth Date: {person.birthDate}</Typography>
-                    {person.captain && <Typography variant="h5" color="primary">Captain</Typography>}
-                    <Typography variant="h5" color="primary">Age: {person.currentAge}</Typography>
-                    <Typography variant="h5" color="primary">Height: {person.height}</Typography>
-                    <Typography variant="h5" color="primary">Nationality: {person.nationality}</Typography>
-                    <Typography variant="h5" color="primary">Jersey Number: #{person.primaryNumber}</Typography>                                      
+                    <Typography variant="h5">Name: {person.fullName}</Typography>
+                    <Typography variant="h5">Birth City: {person.birthCity}</Typography>
+                    <Typography variant="h5">Birth Date: {person.birthDate}</Typography>
+                    {person.captain && <Typography variant="h5">Captain</Typography>}
+                    <Typography variant="h5">Age: {person.currentAge}</Typography>
+                    <Typography variant="h5">Height: {person.height}</Typography>
+                    <Typography variant="h5">Nationality: {person.nationality}</Typography>
+                    <Typography variant="h5">Jersey Number: #{person.primaryNumber}</Typography>                                      
        
                     </Card>
                 </div>
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import Player from './Player'
 
 const useStyles = makeStyles({
@@ -11,45 +11,52 @@ const useStyles = makeStyles({
     playerContainer: {
     },
     playerListContainer: {
-        height: '750px',
         width: '100%',
         minWidth:'100px',
+        marginLeft: '50px',
+    },
+    table: {
+        borderCollapse: 'collapse',
+    },
+    td: {
+        border: '1px solid #dddddd',
+        textAlign: 'left',
+        padding: '8px',
+    },
+    columnTitle: {
+        fontWeight: 'bold',
     }
 });
 
 function PlayerList(props) {
     const classes = useStyles();
     const { playerList } = props
-    const [showOnePlayer, setShowOnePlayer] = useState(false)
     const [playerID, setPlayerID] = useState(undefined)
 
-    const generateRows = (playerList) => {
-        const rows = []
-        for (var i = 0; i < playerList.length; i++) {
-            rows.push({
-                id: playerList[i].person.id,
-                fullName: playerList[i].person.fullName,
-                jerseyNumber: playerList[i].jerseyNumber,
-                position: playerList[i].position.name,
-            })
-        }
-        return rows
-    }
-    const columns = [
-        { field: 'fullName', headerName: 'Full name', width: 260 },
-        { field: 'jerseyNumber', headerName: 'Jersey number', width: 130 },
-        { field: 'position', headerName: 'Position', width: 260 }
-    ];
-
-    const handleRowClick = e => {
-        console.log(e)
-        setPlayerID(e.data.id)
-    }
     return (
         <div className={classes.root}>
             <div className={classes.playerListContainer}>
-                {playerList.length > 0 && <DataGrid rows={generateRows(playerList)} columns={columns} pageSize={playerList.length} onRowClick={handleRowClick} />}
-
+                <table className={classes.table}>
+                <tr className={classes.td}>
+                    <th><Typography className={classes.columnTitle}>Jersey number</Typography></th>
+                    <th><Typography className={classes.columnTitle}>Full name</Typography></th>
+                    <th><Typography className={classes.columnTitle}>Position</Typography></th>                    
+                </tr>
+            
+                {playerList.length > 0 && playerList.map(player => 
+                <tr className={classes.td}>
+                    <th>
+                        <Typography>#{player.jerseyNumber}</Typography>
+                    </th>
+                    <th>
+                        <Typography>{player.person.fullName}</Typography>
+                    </th>
+                    <th>
+                        <Typography>{player.position.name}</Typography>
+                    </th>                
+                </tr>)}
+                
+                </table>
             </div>
             <div className={classes.playerContainer}>
                 {playerID !== undefined && <Player playerID={playerID}></Player>}
